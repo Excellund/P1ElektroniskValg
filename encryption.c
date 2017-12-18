e2df7b03646540b72feaaa72c8e6a308dedaa9b3
xprovoke <mikela@outlook.dk>
2017-12-18 09:06:52 +0100

    finalizing

--- encryption.c
+++ encryption.c
@@ -1,4 +1,6 @@
-
+/* This c-file executes a program, which can manage data given as a text file
+   and encrypt it with a key symmetrically. At a given time it will then de-
+   crypt it using the same key. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -13,169 +15,205 @@
 #define FALSE 0
 #define CPR_LEN 10
 #define DATA_LEN 4
+#define NEWLINE_SIZE 2
+#define HEX_SIZE 2
+#define HEX_BASE 16
 #define DECRYPT_YEAR 2017
 #define DECRYPT_MONTH 12
 #define DECRYPT_DAY 15
-#define DECRYPT_HOUR 11
-#define DECRYPT_MINUTE 39
+#define DECRYPT_HOUR 15
+#define DECRYPT_MINUTE 43
 #define EXIT_SUCCES 0
 
 /* Prototypes */
-void stringToHex(char data[DATA_LEN], char data_hex[DATA_LEN * 2]);
+void stringToHex(char data[DATA_LEN], char data_hex[DATA_LEN * HEX_SIZE]);
 int hexToInt(char);
 int hexToAscii(char, char);
-void hexToString(char data[DATA_LEN], char data_hex[DATA_LEN * 2]);
+void hexToString(char data[DATA_LEN], char data_hex[DATA_LEN * HEX_SIZE]);
 char *generateKey(char *);
-void encrypt(FILE **, char key[DATA_LEN*2], char data_hex[DATA_LEN*2]);
-void decrypt(FILE *, FILE *, char key[DATA_LEN*2]);
+void encrypt(FILE **, char key[DATA_LEN * HEX_SIZE], char data_hex[DATA_LEN * HEX_SIZE]);
+void decrypt(FILE *, FILE *, char key[DATA_LEN * HEX_SIZE]);
 
 /* Main Function */
 int main(void) {
-  char c, key[DATA_LEN*2], data[DATA_LEN+1], data_hex[DATA_LEN*2], data_enc[DATA_LEN*2], data_dec[DATA_LEN*2], temp_string[2], data_asc[DATA_LEN+1];
+  char c, key[DATA_LEN * HEX_SIZE], data[DATA_LEN + 1], data_hex[DATA_LEN * HEX_SIZE], data_enc[DATA_LEN * HEX_SIZE], data_dec[DATA_LEN * HEX_SIZE], temp_string[2], data_asc[DATA_LEN + 1];
   unsigned int ch1, ch2;
   int i = 0, length, j, buf, size, k, current, count = 0;
   time_t t;
   struct tm tm;
 
-  t = time(NULL);
-  srand(time(NULL));
-
-  generateKey(key);
-
+  /* Initialize file pointers in read and write mode */
   FILE *rp = fopen("results", "r+");
   FILE *dp = fopen("data_encrypted", "r+");
   FILE *dp_decrypt = fopen("data_decrypted", "r+");
 
+  /* Prepare use of time */
+  t = time(NULL);
+  srand(time(NULL));
+
+  /* Generate key for encryption */
+  generateKey(key);
+
+  /* Run until the voting phase has finished */
   while (TRUE) {
 
+    /* Get the current time */
     t = time(NULL);
     tm = *localtime(&t);
-    printf("%d %d\n", tm.tm_min, tm.tm_sec);
+
+    /* if the voting phase is over */
     if (tm.tm_year + 1900 >= DECRYPT_YEAR  &&
         tm.tm_mon  + 1    >= DECRYPT_MONTH &&
         tm.tm_mday        >= DECRYPT_DAY   &&
-        tm.tm_hour        >= DECRYPT_HOUR &&
+        tm.tm_hour        >= DECRYPT_HOUR  &&
         tm.tm_min         >= DECRYPT_MINUTE) {
 
+      /* Get amount of encrypted data */
       fseek(dp, 0, SEEK_END);
       size = ftell(dp);
 
+
       fseek(dp, 0, SEEK_SET);
-      for (k = 0; k < size; k += DATA_LEN*2) {
-        for (i = 0; i < DATA_LEN*2; i++) {
+
+      /* For all data in file */
+      for (k = 0; k < size; k += DATA_LEN * HEX_SIZE) {
+
+        /* Get data from a single vote */
+        for (i = 0; i < DATA_LEN * HEX_SIZE; i++) {
           c = getc(dp);
           data_enc[i] = c;
         }
 
-        for (i = 0; i < DATA_LEN*2; i += 2) {
-          strncpy(temp_string, data_enc + i, 2);
+        /* XOR cipherfile and key to get decrypted data as hexadecimal */
+        for (i = 0; i < DATA_LEN * HEX_SIZE; i += HEX_SIZE) {
+          strncpy(temp_string, data_enc + i, HEX_SIZE);
           sscanf(temp_string, "%02X", &ch1);
-          strncpy(temp_string, key + i, 2);
+          strncpy(temp_string, key + i, HEX_SIZE);
           sscanf(temp_string, "%02X", &ch2);
           sprintf(data_dec, "%02X", ch1^ch2);
 
+          /* Convert to ASCII */
           length = strlen(data_dec);
           for(j = 0; j < length; j++){
-            if(j % 2 != 0){
+            if(j % HEX_SIZE != 0){
               data_asc[0] = (char)hexToAscii(buf, data_dec[j]);
             }else{
               buf = data_dec[j];
             }
           }
+
+          /* Write data to file */
           fprintf(dp_decrypt, "%c", data_asc[0]);
         }
         fprintf(dp_decrypt, "%c", '\n');
       }
       printf("Decrypted.\n");
+
+      /* Close files */
       fclose(dp_decrypt);
       fclose(dp);
       fclose(rp);
       break;
+
+    /* If it isn't time to decrypt */
     } else {
-      fseek(rp, count * (DATA_LEN + 2), SEEK_SET);
-      fseek(dp, count * (DATA_LEN * 2), SEEK_SET);
+
+      /* Position pointer to file */
+      fseek(rp, count * (DATA_LEN + NEWLINE_SIZE), SEEK_SET);
+      fseek(dp, count * (DATA_LEN * HEX_SIZE), SEEK_SET);
       current = ftell(rp);
       fseek(rp, 0, SEEK_END);
+
+      /* If there is data left in the file */
       if (!(ftell(rp) - current < 1)) {
-        printf("%d\n", current);
+
+        /* Scan data from results file created by vote.c */
         fseek(rp, current, SEEK_SET);
         strcpy(data, "");
         fscanf(rp, "%[^\n]\n", data);
 
-        printf("%s\n", data);
-
+        /* Convert to hexadecimal */
         stringToHex(data, data_hex);
+
+        /* Encrypt the data */
         encrypt(&dp, key, data_hex);
+
+        /* Move down a line in the files */
         count++;
       }
     }
   }
 
+  /* When done decrypting: Close files */
   fclose(dp);
   fclose(rp);
 
   return EXIT_SUCCES;
 }
 
+/* Generates a key in hexadecimal form */
 char *generateKey(char *key) {
   const char charset[] = "0123456789ABCDEF";
   int i, charset_index;
 
-  for (i = 0; i < DATA_LEN * 2; i++) {
+  for (i = 0; i < DATA_LEN * HEX_SIZE; i++) {
     charset_index = rand() % (int) (sizeof charset - 1);
     key[i] = charset[charset_index];
   }
-  key[DATA_LEN * 2] = '\0';
+  key[DATA_LEN * HEX_SIZE] = '\0';
 
   return key;
 }
 
-void stringToHex(char data[DATA_LEN], char data_hex[DATA_LEN * 2]) {
+/* Converts a char array of ASCII to char array of hexadecimal */
+void stringToHex(char data[DATA_LEN], char data_hex[DATA_LEN * HEX_SIZE]) {
   int i;
   for(i = 0; i < DATA_LEN; i++){
-    sprintf((data_hex + i * 2), "%02X", data[i]);
+    sprintf((data_hex + i * HEX_SIZE), "%02X", data[i]);
   }
-  data_hex[DATA_LEN * 2] = '\0';
+  data_hex[DATA_LEN * HEX_SIZE] = '\0';
 }
 
 int hexToInt(char c){
-        int first = c / 16 - 3;
-        int second = c % 16;
-        int result = first * 10 + second;
-        if(result > 9) {
-          result--;
-        }
-        return result;
+  int first = c / HEX_BASE - 3;
+  int second = c % HEX_BASE;
+  int result = first * 10 + second;
+  if(result > 9) {
+    result--;
+  }
+  return result;
 }
 
 int hexToAscii(char a, char b){
-  int high = hexToInt(a) * 16;
+  int high = hexToInt(a) * HEX_BASE;
   int low = hexToInt(b);
   return high + low;
 }
 
-void hexToString(char data[DATA_LEN], char data_hex[DATA_LEN * 2]) {
+/* Converts a char array of hexadecimal to char array of ASCII */
+void hexToString(char data[DATA_LEN], char data_hex[DATA_LEN * HEX_SIZE]) {
   int length = strlen(data_hex);
   int i;
   char buf = 0;
   for(i = 0; i < length; i++){
-    if(i % 2 != 0){
-      data[i / 2] = hexToAscii(buf, data_hex[i]);
+    if(i % HEX_SIZE != 0){
+      data[i / HEX_SIZE] = hexToAscii(buf, data_hex[i]);
     }else{
       buf = data_hex[i];
     }
   }
 }
 
-void encrypt(FILE **dp, char key[DATA_LEN*2], char data_hex[DATA_LEN*2]) {
-  char temp_string[2];
+/* Encrypt data using a key, and print the outpput to a textfile */
+void encrypt(FILE **dp, char key[DATA_LEN * HEX_SIZE], char data_hex[DATA_LEN * HEX_SIZE]) {
+  char temp_string[HEX_SIZE];
   unsigned int ch1, ch2;
   int i;
 
-  for (i = 0; i < DATA_LEN*2; i += 2) {
-    strncpy(temp_string, data_hex + i, 2);
+  for (i = 0; i < DATA_LEN * HEX_SIZE; i += HEX_SIZE) {
+    strncpy(temp_string, data_hex + i, HEX_SIZE);
     sscanf(temp_string, "%02X", &ch1);
-    strncpy(temp_string, key + i, 2);
+    strncpy(temp_string, key + i, HEX_SIZE);
     sscanf(temp_string, "%02X", &ch2);
     fprintf(*dp, "%02X", ch1^ch2);
   }
--- vote.c
+++ vote.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -13,6 +12,8 @@
 #define FALSE 0
 #define CPR_LEN 10
 #define DATA_LEN 4
+#define MONTHS_IN_YEAR 12
+#define NEWLINE_SIZE 2
 #define EXIT_SUCCES 0
 
 /* Prototypes */
@@ -26,32 +27,41 @@ int main(void) {
   char CPR[CPR_LEN+1], person[2], party[2], data[DATA_LEN+1];
   int position = 0;
 
+  /* Initialize file pointers in read and write mode */
   FILE *cp = fopen("identification", "r+");
   FILE *rp = fopen("results", "r+");
 
+
   while (TRUE) {
+
+    /* Promt for identification */
     printf("Indtast venligst dit CPR-nummer: ");
     strcpy(CPR, "");
-
     scanf(" %s", CPR);
 
+    /* check identity authenticity */
     if (!verifyIdentity(CPR)) {
       printf("Incorrect ID.\n");
     } else {
+
+      /* Checker whether the user has voted earlier. */
       if (BinarySearch(&cp, CPR, &position)) {
         printf("You have already voted and aren't allowed to vote again.\n");
       } else {
         moveFileText(&cp, position, CPR);
 
+        /* Promt for voting data */
         printf("Angiv venligst din stemme for parti og person: ");
         scanf(" %[A-Z]", party);
         scanf(" %[A-Z]", person);
 
+        /* Collects the inputs in a single data variable */
         strcpy(data, "");
         strcat(data, party);
         strcat(data, ", ");
         strcat(data, person);
 
+        /* Print the data to the results file */
         fseek(rp, 0, SEEK_END);
         fprintf(rp, "%s\n", data);
 
@@ -60,35 +70,48 @@ int main(void) {
     }
   }
 
+  /* Close opened files */
   fclose(cp);
   fclose(rp);
   return EXIT_SUCCES;
 }
 
+/* Function that checks if the identification is valid */
 int verifyIdentity(char *CPR) {
 
   char CPR_Splittet[4][5];
   int i = 0;
 
+  /* Check if the input was too long */
   if (strlen(CPR) > CPR_LEN) {
     return FALSE;
   }
 
+  /* Split the CPR in it's parts */
   for (i = 0; i < CPR_LEN - 2 ; i += 2) {
     strncpy(CPR_Splittet[i/2], CPR + i, i == 6 ? 4 : 2);
     CPR_Splittet[i/2][i == 6 ? 4 : 2] = 0;
   }
 
-  for (i = 0; i < 12; i++) {
-    if (atoi(CPR_Splittet[1]) > 12 || atoi(CPR_Splittet[1]) < 1) {
+  /* For all the months in a year */
+  for (i = 0; i < MONTHS_IN_YEAR; i++) {
+
+    /* If the amount is not within the interval */
+    if (atoi(CPR_Splittet[1]) > MONTHS_IN_YEAR || atoi(CPR_Splittet[1]) < 1) {
       return FALSE;
+
+    /* if the month is february */
     } else if (atoi(CPR_Splittet[1]) == 2) {
+      /* Check if the day is under the amount of days that year */
       if (atoi(CPR_Splittet[0]) > 28 + isLeapYear(CPR_Splittet[2], CPR_Splittet[3][0])) {
         return FALSE;
       }
     }
+    /* For all the other months */
     else {
+      /* Find the correct month */
       if (atoi(CPR_Splittet[1]) == i) {
+        /* Check how many days are in the month and check if the input is valid */
         if (atoi(CPR_Splittet[0]) > 30 + ((i <= 7) ? (i % 2) : ((i - 1) % 2))) {
           return FALSE;
         }
@@ -96,18 +119,22 @@ int verifyIdentity(char *CPR) {
     }
   }
 
+  /* If everything is ok, return TRUE */
   return TRUE;
 }
 
+/* Function data returns a boolean value to determine if a year is a leapyear */
 int isLeapYear(char * yearLastDigits, char ciffer) {
   char yearString[4];
   int cifferInt;
   int yearLastDigitsInt;
   int year;
 
+  /* Convert the chars to int */
   yearLastDigitsInt = atoi(yearLastDigits);
   cifferInt = ciffer - '0';
 
+  /* Determine the century */
   if (cifferInt >= 5 && cifferInt <= 8 && yearLastDigitsInt >= 58) {
     strcpy(yearString, "18");
   } else if (cifferInt <= 3 || ((cifferInt == 4 || cifferInt == 9) && yearLastDigitsInt >= 37)) {
@@ -116,70 +143,99 @@ int isLeapYear(char * yearLastDigits, char ciffer) {
     strcpy(yearString, "20");
   }
 
+  /* Get the resulting year */
   strcpy(yearString + 2, yearLastDigits);
   year = atoi(yearString);
 
+  /* Check for leapyear */
   if ( (year % 4 == 0) && ( year % 100 != 0 || year % 400 == 0 ))
     return 1;
   else
     return 0;
 }
 
+/* Performs a binary search to find out if the CPR already has been used to vote */
 int BinarySearch(FILE **cp, char *CPR, int *position) {
-
   int current_line, upper, lower = 0;
   double CPR_number, CPR_check;
   char CPR_check_string[CPR_LEN];
 
+  /* Stores the CPR as a double */
   sscanf(CPR, "%lf", &CPR_number);
 
+  /* Determines the upper point */
   fseek(*cp, 0L, SEEK_END);
-  upper = ftell(*cp)-12;
+  upper = ftell(*cp) - (CPR_LEN + NEWLINE_SIZE);
 
+  /* While still searching */
   while (lower <= upper) {
-    current_line = (lower + (upper - lower) / 2) + (lower + (upper - lower) / 2) % 12;
 
+    /* Caclulate the middle line */
+    current_line = (lower + (upper - lower) / 2) + (lower + (upper - lower) / 2) % (CPR_LEN + NEWLINE_SIZE);
+
+    /* Position the file pointer */
     fseek(*cp, current_line, SEEK_SET);
 
+    /* Scan the CPR */
     fscanf(*cp, "%[0-9]", CPR_check_string);
 
+    /* Convert this CPR to a double aswell */
     sscanf(CPR_check_string, "%lf", &CPR_check);
 
+    /* if the CPRs are equivalent */
     if (CPR_number == CPR_check) {
       *position = current_line;
       return TRUE;
+
+    /* if they aren't */
     } else {
+      /* Check if the original CPR value is 'less' than the checked one */
       if (CPR_number < CPR_check) {
-        upper = current_line - 12;
+        /* reposition the upper index */
+        upper = current_line - (CPR_LEN + NEWLINE_SIZE);
+      /* Check if the original CPR value is 'larger' than the checked one */
       } else if (CPR_number > CPR_check) {
-        lower = current_line + 12;
+        /* reposition the lower index */
+        lower = current_line + (CPR_LEN + NEWLINE_SIZE);
         if (lower >= upper) {
-          current_line += 12;
+          current_line += (CPR_LEN + NEWLINE_SIZE);
         }
       }
     }
   }
 
+  /* Set the position */
   *position = current_line;
 
   return FALSE;
 }
 
+/* Function that uses a temporary file to move data instead of overwriting it */
 void moveFileText(FILE **cp, int position, char *CPR) {
   char c;
+
+  /* Open temporary file */
   FILE *cp_temp = fopen("cpr_temp", "w+");
   fseek(*cp, position, SEEK_SET);
+
+  /* Write all data after the position to a temporary file */
   while ((c = getc(*cp)) != EOF) {
     putc(c, cp_temp);
   }
   fseek(*cp, position, SEEK_SET);
 
+  /* Write the CPR at the desired position */
   fwrite(CPR, 1, strlen(CPR), *cp);
-  fseek(*cp, position + 12, SEEK_SET);
+
+  /* Reposition pointers */
+  fseek(*cp, position + (CPR_LEN + NEWLINE_SIZE), SEEK_SET);
   fseek(cp_temp, 0, SEEK_SET);
 
+  /* Write everything from the temporary file, back in the original file again */
   while ((c = getc(cp_temp)) != EOF) {
     putc(c, *cp);
   }
+
+  /* Close the file */
   fclose(cp_temp);
 }
